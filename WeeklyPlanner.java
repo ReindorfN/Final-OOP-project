ce708f808be8d4f200b49571045f4b00c9e7b3f4
Lady-Margaret (Marge) Hagan <144915421+ladymargehagan@users.noreply.github.com>
2024-04-13 20:02:16 +0000

    Update WeeklyPlanner.java

--- WeeklyPlanner.java
+++ WeeklyPlanner.java
@@ -1,17 +1,21 @@
-    //WEEKLY PLANNER CLASS
-    public class WeeklyPlanner{
-		   public String[] days={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
-    
-    public WeeklyPlanner(String days ){
-        weeklyTimetable= new WeeklyPlanner[7]; 
+public class WeeklyPlanner {
+    private String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    private DailyPlanner[] weeklyTimetable = new DailyPlanner[7]; 
+
+    public WeeklyPlanner() {
+        for (int i = 0; i < weeklyTimetable.length; i++) {
+            weeklyTimetable[i] = new DailySchedule(days[i]);
+        }
     }
 
-    //class methods
-    public void displayWeeklyTimetable(){
-	    System.out.println("Schedule for the Week");
-		   for(int i=-1;i<7;i++){
-				   System.out.println("Schedule for" + days[i]);
-				   weeklyTimetable[i].displayDailyAppointments();
-			}
-	  }
-	}    
+    public void displayWeeklyTimetable() {
+	    try {
+            System.out.println("Schedule for the week:");
+            for (int i = 0; i < days.length; i++) {
+                System.out.println("Schedule for " + days[i] + ":");
+                weeklyTimetable[i].displayDailyAppointments(); 
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("The array index is out of bounds");
+        }
+    }
