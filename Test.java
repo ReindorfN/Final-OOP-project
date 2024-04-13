1f108d10192211c7f4faac051d9cac0d8f3384d0
ReindorfN <reindorfnarh@gmail.com>
2024-04-13 19:45:32 +0000

    yhh

--- Test.java
+++
@@ -1,7 +0,0 @@
-public class Test{
-    public static void main(String... args){
-        Student student1 = new Student("Ama", 4543, "MIS", "helloworld");
-        student1.getStudentName();
-        System.out.print(student1.getStudentName());
-    }
-}
