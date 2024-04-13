b5962ccf483810170f750d9c3fa02960b6e5305c
maame-dankwaa <144915734+maame-dankwaa@users.noreply.github.com>
2024-04-13 20:51:54 +0000

    Add files via upload

---
+++ Subject.java
@@ -0,0 +1,46 @@
+public class Subject extends Event {
+
+    //class specific instance variables
+    private int numModules;
+   private ArrayList<String> modules = new ArrayList<String>(); 
+   private double progress;
+
+   public Subject(String subjectName, String time, ArrayList<String> modules, double progress) {
+       super(subjectName, time); // calling the constructor of the main event abstract class 
+       this.modules = modules;
+       this.progress = progress;
+   }
+
+   public Subject(String subjectName, String time){
+    super(subjectName, time);
+   }
+
+   public ArrayList<String> getModules() { //getter 
+       return modules;
+   }
+   
+   public String getModule(int n){
+           this.modules.get(n);
+   }
+
+   public void setModules(ArrayList<String> modules) {  //setter
+       this.modules = modules;
+   }
+
+   public double getProgress() { //getter
+       return progress;
+   }
+
+   public void setProgress(double progress) { //setter 
+       this.progress = progress;
+   }
+
+   // Implementation of the abstract method from Event
+   @Override
+   public void displayEventDetails() {
+       System.out.println("Subject Name: " + this.eventName);
+       System.out.println("Time: " + this.eventTime);
+       System.out.println("Modules: " + this.modules);
+       System.out.println("Progress: " + this.progress + "%");
+   }
+}
